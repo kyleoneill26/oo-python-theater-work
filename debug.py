38a178c303c101c6c2d378d9890b99b41e60f81c
Kyle <kyleoneill26@gmail.com>
2023-03-26 20:51:33 -0500

    progress through lab

--- debug.py
+++ debug.py
@@ -2,6 +2,13 @@ import ipdb
 from lib import *
 
 # Test your code below...
+batman = Role("Batman")
+spiderman = Role("SPiderman")
+
+a1 = Audition("Michael", "LA", True, batman)
+a2 = Audition("Alex", "Nashville", True, batman)
+a2 = Audition("James", "NYC", False, spiderman)
+a2 = Audition("Rhianna", "Miami", True, spiderman)
 
 
 
--- lib/audition.py
+++ lib/audition.py
@@ -1,3 +1,14 @@
 
 class Audition:
-    pass
+    all = []
+    def __init__(self, actor, location, hired, role):
+        self.actor = actor
+        self.location = location
+        self.hired = hired
+        self.role = role
+        Audition.all.append( self )
+
+    def call_back( self):
+        self.hired = True
+
+    
